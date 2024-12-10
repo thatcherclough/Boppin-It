5a27b8c5335d35871c353677edc575c5e288b826
Finn Borgnes <borgnes@wisc.edu>
2024-12-10 17:35:08 -0600

    Music
    Music is working and can settings can adjust its volume and turn it off.

--- app/src/main/java/com/cs407/boppinit/MainActivity.kt
+++ app/src/main/java/com/cs407/boppinit/MainActivity.kt
@@ -32,6 +32,12 @@ class MainActivity : AppCompatActivity() {
 
         setupClickListeners()
         setupToggleGroups()
+
+        // Initialize audio manager
+        AudioManager.initialize(this)
+
+        // Play music
+        AudioManager.playMusic()
     }
 
     private fun setupClickListeners() {
@@ -121,5 +127,10 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra(GameProps.DIFFICULTY.name, selectedDifficulty.name)
             startActivity(intent)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        AudioManager.stopMusic()
     }
 }
